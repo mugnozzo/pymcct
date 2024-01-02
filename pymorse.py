5d2c154bcc9537c1d5698baa24907f75bb77d201
Alek Mugnozzo <mugnozzo@gmail.com>
2024-01-02 23:12:39 +0100

    Added output file management by argument

--- pymorse.py
+++ pymorse.py
@@ -1,8 +1,20 @@
 #!/bin/python
 
 import morse_tools as mt
+import argparse     # to parse arguments passed to the command
+import os.path # to manage input file
 
-text = "Hello"
+# Parsing arguments
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-o","--output",help = "output wave file (when generating sound)",required = False)
+
+args = argParser.parse_args()
+
+output_file="morse_sound.wav"
+if output_file is not None:
+    output_file = args.output
+
+text = "jukni tirxu"
 morse_translation = mt.text_to_morse(text)
 print(morse_translation)
-mt.morse_to_sound(morse_translation)
+mt.morse_to_sound(morse_translation, output_file=output_file)
